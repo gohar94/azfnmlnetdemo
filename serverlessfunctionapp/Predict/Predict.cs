0939db3b7f454cfd82d9fd7def70aa757108df4e
Gohar Irfan Chaudhry <gochaudh@microsoft.com>
2020-03-31 14:36:21 -0700

    Changes to make it run on Functions v3

--- serverlessfunctionapp/Predict/Predict.cs
+++ serverlessfunctionapp/Predict/Predict.cs
@@ -7,6 +7,7 @@ using Microsoft.Azure.WebJobs.Host;
 using Newtonsoft.Json;
 using Microsoft.ML;
 using serverlessfunctionapp.Predict;
+using Microsoft.Extensions.Logging;
 
 namespace Predict
 {
@@ -16,7 +17,7 @@ namespace Predict
         public static IActionResult Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequest req,
             [Blob("models/model.zip", FileAccess.Read, Connection = "AzureWebJobsStorage")] Stream serializedModel,
-            TraceWriter log)
+            ILogger log)
         {
             // Workaround for Azure Functions Host
             if (typeof(Microsoft.ML.Runtime.Data.LoadTransform) == null ||
@@ -24,14 +25,14 @@ namespace Predict
                 typeof(Microsoft.ML.Runtime.Internal.CpuMath.SseUtils) == null ||
                 typeof(Microsoft.ML.Runtime.FastTree.FastTree) == null)
             {
-                log.Error("Error loading ML.NET");
+                log.LogError("Error loading ML.NET");
                 return new StatusCodeResult(500);
             }
 
             //Read incoming request body
             string requestBody = new StreamReader(req.Body).ReadToEnd();
 
-            log.Info(requestBody);
+            log.LogInformation(requestBody);
 
             //Bind request body to IrisData object
             IrisData data = JsonConvert.DeserializeObject<IrisData>(requestBody);
